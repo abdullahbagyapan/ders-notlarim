ef3be5c6202e7085cc400c2db6b49d2fbd5b35a6
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2023-12-30 16:21:34 +0300

    3.odev cozumleri eklendi

--- veri-yapilari/odev-3/Cuckoo.java
+++ veri-yapilari/odev-3/Cuckoo.java
@@ -6,15 +6,67 @@ public class Cuckoo extends AbstractCuckoo{
         super(max_rehash_attempts, table1, table2);
     }
 
-    @Override
-    public boolean insert(int key) {
-        // Metodu yazınız
-        return false;
+    private Integer insertTable1(int key) {
+
+        int hash = table1.hash(key);
+
+        Integer temp = table1.get(hash); 
+        table1.set(hash, key);
+
+        return temp;
+    }
+    private Integer insertTable2(int key) {
+
+        int hash = table2.hash(key);
+
+        Integer temp = table2.get(hash); 
+        table2.set(hash, key);
+
+        return temp;
     }
 
     @Override
+    public boolean insert(int key) {
+
+        int i = 0;
+        int insertAttempts = 0;
+
+        Integer temp = key;
+
+        while (insertAttempts < MAX_REHASH_ATTEMPTS) {
+            
+            if (i % 2 == 0) {
+                temp = insertTable1(temp);
+            }
+            else {
+                temp = insertTable2(temp);
+            }
+
+            if (temp == null) {
+                return true;
+            }
+
+            insertAttempts++;
+            i++;
+        }
+        return false;
+    }
+    @Override
     public boolean search(int key) {
-        // Metodu yazınız
+    
+        int hash1 = table1.hash(key);
+        Integer value = table1.get(hash1);
+
+        if (value != null && value == key) {
+            return true;
+        }
+
+        int hash2 = table2.hash(key);
+        value = table2.get(hash2);
+
+        if (value != null && value == key) {
+            return true;
+        }
         return false;
     }
 
